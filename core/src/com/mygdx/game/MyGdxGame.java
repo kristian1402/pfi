044e7315175654425911e19c769f3497a968d3f0
kristian1402 <ksaren20@student.aau.dk>
2021-04-09 17:21:04 +0200

    pfi

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,27 +2,44 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
+	Music jumpSound;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,800,480);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.png");
+		jumpSound = Gdx.audio.newMusic(Gdx.files.internal("jump.mp3"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
+		camera.update();
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+			jumpSound.play();
+		}
 	}
 	
 	@Override
