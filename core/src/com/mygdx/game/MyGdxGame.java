81092e086e7955bb0d03c399b800447485765a36
kristian1402 <ksaren20@student.aau.dk>
2021-04-13 21:10:45 +0200

    pfi

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.player.player;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -16,53 +19,48 @@ public class MyGdxGame extends ApplicationAdapter {
 	Texture img; //For the background image
 	Music jumpSound;
 	Texture playerSprite;
-	com.mygdx.game.player.player player;
-	int backgroundOffset;
+	player player;
+	int backgroundOffset = 1;
+	ShapeRenderer shapeRenderer; //bruges til at lave en kasse rundt om player, så man kan se kollisioner. kun midlertidig
 
 	
 	@Override
 	public void create() {
+		player = new player(400,300);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
-		img = new Texture("background.png");
+		img = new Texture("backgroundimage.png");
 		jumpSound = Gdx.audio.newMusic(Gdx.files.internal("jump.mp3"));
 		playerSprite = new Texture(Gdx.files.internal("player1.png"));
-		player = new player();
-
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render() {
+		player.update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 		camera.update();
 			//Begins batch and draws sprites
 		backgroundOffset++;
-		if (backgroundOffset % Gdx.graphics.getWidth() == 0){
+		if (backgroundOffset > Gdx.graphics.getWidth()){
 			backgroundOffset = 0;
 		}
 
 		batch.begin();
-		batch.draw(img,-backgroundOffset,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(playerSprite, player.getPlayerX(), player.getPlayerY(),64,128);
+		batch.draw(img,-backgroundOffset,0);
+		batch.draw(playerSprite, player.getPlayerX(),player.getPlayerY(),64,128);
 		batch.end();
 
-		/*	//User input
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			player.x -= 50; Gdx.graphics.getDeltaTime();
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			player.x += 50; Gdx.graphics.getDeltaTime();
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-			jumpSound.play();
-		}
-		*/
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.rect(player.getPlayerX()+8, player.getPlayerY(), 50,128);
+		shapeRenderer.end();
 
 	}
-	
+
+
 	@Override
 	public void dispose() {
 		img.dispose();
--- core/src/com/mygdx/game/player/player.java
+++ core/src/com/mygdx/game/player/player.java
@@ -3,6 +3,7 @@ package com.mygdx.game.player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -11,12 +12,14 @@ public class player {
     Texture playerSprite;
     Rectangle player;
     Vector2 pos;
+    Texture texture;
+
     float velocityY = 0;
     boolean grounded;
     float jumpVelocity = 5;
     float weight;
-    int playerX = 341;
-    int playerY =150;
+    int playerX = 150;
+    int playerY = 150;
 
     public int getPlayerX() {
         return playerX;
@@ -26,8 +29,9 @@ public class player {
         return playerY;
     }
 
-    public void setPlayer(int playerX, int playerY) {
+    public player(int playerX, int playerY) {
         playerSprite = new Texture(Gdx.files.internal("player1.png"));
+        texture = new Texture(Gdx.files.internal("playeranimation.png"));
         player = new Rectangle();
         player.x = playerX;
         player.y = playerY;
@@ -35,35 +39,28 @@ public class player {
         player.height = 64;
         weight = 0.2f;
         this.pos = new Vector2(340,300);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            player.x -= 50; Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            player.x += 50; Gdx.graphics.getDeltaTime();
+        }
     }
 
     public float getWeight() {
         return weight;
     }
 
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public void update(float deltaTime){
-        if (playerY == 150){
-            grounded = true;
-        } else{
-            grounded = false;
-        }
+    public void update(){
+        grounded = playerY == 150;
 
         if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && grounded)
             playerY += jumpVelocity * getWeight();
         else if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !grounded && this.velocityY > 0)
-            playerY += jumpVelocity * getWeight() * deltaTime;
+            playerY += jumpVelocity * getWeight() * Gdx.graphics.getDeltaTime();
 
 
     }
-
-
-
-
-
-
 
 }
